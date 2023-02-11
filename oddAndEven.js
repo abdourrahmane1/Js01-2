a0178c3ef2c837e53c22b6092ed0d437c48eba6c
abdourrahmane1 <105205890+abdourrahmane1@users.noreply.github.com>
2023-02-11 14:54:56 +0100

    Update oddAndEven.js

--- oddAndEven.js
+++ oddAndEven.js
@@ -1,16 +1,10 @@
-//Déclaration et Initialisation de la variable sur lequel va porter le test
-let nombre = 1;
-// une boucle while pour tester à quel moment arreter l'exécution
-while (nombre <= 20)
-  {
-   // une boucle if pour tester si le nombre est impaire ou paire et l'afficher
-        if (nombre % 2 != 0)
-        {
-            console.log("Impaire");
-        }
-        else
-        {
-            console.log("Paire");
-        }
-        nombre++;
-  }
+let i = 1;
+while (i <=20) {
+    if (i % 2 != 0) {
+        console.log("Impaire");
+    }
+    else{
+        console.log("Paire")
+    }
+    i+= 1;
+}
